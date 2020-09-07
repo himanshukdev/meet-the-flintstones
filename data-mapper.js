a009f014b332c0fdb56e567b64930faad2e922f6
Himanshu Khatri <himanshuk.dev@gmail.com>
2020-09-07 15:36:52 +0530

    challenge solution

--- data-mapper.js
+++ data-mapper.js
@@ -5,7 +5,21 @@ const { getCars, getPeople } = services;
 function datamapper() {
 
   this.getPersonsCars = function getPersonsCars(personId) {
-    
+
+    let person;
+    let personCars = [];
+
+    return getPeople().then(list => {
+      person = list.find(x =>  x.id === personId);
+      return getCars().then(car_list => {
+        personCars = person.cars.map(carId => car_list.find(x => x.id === carId));
+        const carsObject =[];
+        personCars.sort((a, b) => a.id - b.id ).forEach(car => {
+          if(car) carsObject.push(`${car.year} ${car.make} ${car.model}`);
+        });
+        return { id: person.id, name: person.name, cars: carsObject }
+      });
+    });
   }
 }
 
